2073d3792d86216801429d33e52d0be9eb6d39a8
ArtemTomin1989 <silovik1989@gmail.com>
2025-04-07 11:59:36 +0300

    method post for login, hashing and comparing pass

--- routes/auth_routes.js
+++ routes/auth_routes.js
@@ -1,5 +1,6 @@
 const { Router } = require("express");
 const db_user = require("../models/user");
+const bcrypt = require("bcryptjs");
 
 const router = new Router();
 
@@ -13,9 +14,10 @@ router.post("/register", async (req, res) => {
   if (user) {
     console.log("такий користувач вже існує");
   } else {
+    const hashedPassword = await bcrypt.hash(password, 10);
     const new_user = new db_user({
       email,
-      password,
+      password: hashedPassword,
     });
     console.log(`Нового користувача ${new_user.email} додано`);
     await new_user.save();
@@ -25,6 +27,26 @@ router.post("/register", async (req, res) => {
 
 router.get("/login", (req, res) => {
   res.render("login.ejs");
+});
+
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await db_user.findOne({ email });
+
+  if (!user) {
+    console.log("Користувач не знайдений");
+    return res.redirect("/auth/login");
+  }
+
+  const is_same = await bcrypt.compare(password, user.password);
+
+  if (!is_same) {
+    console.log("Неправильний пароль");
+    return res.redirect("/auth/login");
+  }
+
+  console.log(`Користувач ${user.email} успішно увійшов`);
+  return res.redirect("/");
 });
 
 module.exports = router;
