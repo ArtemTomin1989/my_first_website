effb085455cdb0e189687ef1c0af0e42993a544e
ArtemTomin1989 <silovik1989@gmail.com>
2025-05-02 11:01:23 +0300

    added edit product option

---
+++ routes/products/edit_my_products_routes.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const router = express.Router();
+
+const db_product = require("../../models/product");
+
+router.get("/:id", async (req, res) => {
+  try {
+    const product = await db_product.findById(req.params.id);
+    return res.render("products/edit_my_products.ejs", { product });
+  } catch (err) {
+    console.error("Edit form error:", err);
+    return res.redirect("/products/edit_my_products", { product });
+  }
+});
+
+router.post("/:id", async (req, res) => {
+  const { name, price, description } = req.body;
+  try {
+    await db_product.findByIdAndUpdate(req.params.id, {
+      name,
+      price,
+      description,
+    });
+
+    return res.redirect("/my_products");
+  } catch (err) {
+    console.error("Update error:", err);
+    return res.redirect("/my_products");
+  }
+});
+module.exports = router;
